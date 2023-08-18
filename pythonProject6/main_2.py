4e9f7d118b11371692717e9233b04e4a7c4bd5b9
L_Dem_L <ostapvasilenko55@gmail.com>
2023-08-18 19:45:27 +0300

    end

--- pythonProject6/main_2.py
+++ pythonProject6/main_2.py
@@ -1,7 +1,6 @@
 import requests
 import json
 
-
 baseURL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
 if __name__ == "__main__":
     response = requests.get(f"{baseURL}")
